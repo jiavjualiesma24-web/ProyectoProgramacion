222a23f8b0899dba71662e084987adb3515b5846
Juanma <jiavju_aliesma24@iesmedinaazahara.es>
2026-04-16 13:30:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1. ACTUALIZA ESTA VARIABLE con la última URL que has copiado de Google
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxckzkXbjUXYYNn0wWP4OQX6PkZvDFmCruZRzNU1S-kyhxr58-jBGjkOyd8qYvMW0I7/exec'
+
+const form = document.forms['mi-formulario']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  
+  const botonEnvio = form.querySelector('button[type="submit"]')
+  
+  // Bloqueamos el botón y ponemos el cursor de prohibido
+  botonEnvio.disabled = true
+  botonEnvio.innerText = "Procesando envío..."
+  botonEnvio.style.cursor = "not-allowed" 
+  botonEnvio.style.opacity = "0.7"
+
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        // Mensaje de éxito
+        alert("¡Solicitud enviada! El equipo de Ethernet Solutions se pondrá en contacto cuanto antes.")
+        
+        // Restablecemos el botón para que vuelva a la normalidad
+        botonEnvio.disabled = false
+        botonEnvio.innerText = "Enviar Datos al Servidor"
+        botonEnvio.style.cursor = "pointer"
+        botonEnvio.style.opacity = "1"
+        
+        form.reset() 
+    })
+    .catch(error => {
+        console.error('Error!', error.message)
+        alert("Error al conectar con Google. Revisa la URL del script.")
+        
+        botonEnvio.disabled = false
+        botonEnvio.innerText = "Reintentar Envío"
+        botonEnvio.style.cursor = "pointer"
+        botonEnvio.style.opacity = "1"
+    })
+})
